db7ffb42686f7d0cc8d77533bf4cbd7328c867dd
Patrick Oser <patrick.oser148@gmail.com>
2023-11-01 15:29:23 -0400

    Finish middleware section.

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const path = require('path');
 const cors = require('cors');
 const { logger } = require('./middleware/logEvents');
-const { errorHandler } = require('./middleware/errorHandler');
+const errorHandler = require('./middleware/errorHandler');
 const PORT = process.env.PORT || 3500;
 
 // custom middleware logger
@@ -74,13 +74,18 @@ const three = (req, res) => {
 
 app.get('/chain(.html)?', [one, two, three]);
 
-app.get('/*', (req, res) => {
-    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
-})
 
-app.use(function (err, req, res, next) {
-    console.error(err.stack)
-    res.status(500).send(err.message);
-})
+app.all('*', (req, res) => {
+    res.status(404)
+    if (req.accepts('html')) {
+        res.sendFile(path.join(__dirname, 'views', '404.html'));
+    } else if (req.accepts('json')) {
+        res.json({ error: "404 NotFound"})
+    } else {
+        res.type('txt').send('404 Not Found');
+    }
+});
+
+app.use(errorHandler);
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));    
