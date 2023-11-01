a5b8c12f9f15d3839b6aa55973c38a7d0a898be0
Patrick Oser <patrick.oser148@gmail.com>
2023-11-01 14:52:00 -0400

    Add errorHandler.js to middleware folder.

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const cors = require('cors');
-const { logger } = require('./middleware/logEvents')
+const { logger } = require('./middleware/logEvents');
+const { errorHandler } = require('./middleware/errorHandler');
 const PORT = process.env.PORT || 3500;
 
 // custom middleware logger
@@ -10,7 +11,18 @@ const PORT = process.env.PORT || 3500;
 app.use(logger);
 
 // Cross Origin Resource Sharing
-app.use(cors());
+const whitelist = ['https://www.google.com', 'http://127.0.0.1:550', 'http://localhost:3500'];
+const corsOptions = {
+    origin: (origin, callback) => {
+        if (whitelist.indexOf(origin) !== -1 || !origin) {
+            callback(null, true)
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
+    optionsSuccessStatus: 200
+}
+app.use(cors(corsOptions));
 
 // built in middleware to handle urlencoded data
 // in other words, form data:
@@ -64,6 +76,11 @@ app.get('/chain(.html)?', [one, two, three]);
 
 app.get('/*', (req, res) => {
     res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+})
+
+app.use(function (err, req, res, next) {
+    console.error(err.stack)
+    res.status(500).send(err.message);
 })
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));    
