c8a959e719954fe16a4477a154ec5a1ff9532831
Patrick Oser <patrick.oser148@gmail.com>
2023-11-01 13:57:46 -0400

    Add logEvents functions to server.js.

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const logEvents = require('./middleware/logEvents')
 const PORT = process.env.PORT || 3500;
 
 // custom middleware logger
 // next() needs to be called because this is not bult in middleware
 app.use((req, res, next) => {
+    logEvents(`${req.method}\t${req.headers.origin}\t${requrl}`, 'reqLog.txt');
     console.log(`${req.method} ${req.path}`);
     next();
 });
