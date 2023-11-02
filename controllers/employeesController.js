e98174161a7bcd2bcd3c5ca320a1fafa4e136b21
Patrick Oser <patrick.oser148@gmail.com>
2023-11-02 10:29:57 -0400

    Add new path 'controllers/employeesController.js'.

---
+++ controllers/employeesController.js
@@ -0,0 +1,26 @@
+const data = {};
+data.employees = require('../../model/employees.json');
+
+const getAllEmployees = (req, res) => {
+    res.json(data.employees)
+}
+
+const createNewEmployee = (req, res) => {
+    res.json({
+        "firstname": req.body.firstname,
+        "lastname": req.body.lastname
+    });
+}
+
+const updateEmployee = (req, res) => {
+    res.json({
+        "firstname": req.body.firstname,
+        "lastname": req.body.lastname
+    });
+}
+
+const deleteEmployee = (req, res) => {
+    res.json({ "id": req.body.id })
+}
+
+module.export = getAllEmployees;
