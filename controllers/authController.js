bb02f9ce82d704e827c6b174464fea650f9eb3f7
Patrick Oser <patrick.oser148@gmail.com>
2023-11-04 12:40:31 -0400

    Add ability to create and assign JWT's.

--- controllers/authController.js
+++ controllers/authController.js
@@ -5,6 +5,11 @@ const usersDB = {
 
 const bcrypt = require('bcrypt');
 
+const jwt = require('jsonwebtoken');
+require('dotenv.config');
+const fsPromises = require('fs').promises;
+const path = require('path');
+
 const handleLogin = async (req, res) => {
     const { user, pwd } = req.body;
     if (!user || !pwd) return res.status(400).json({ 'message': 'Username and password required.' });
@@ -14,6 +19,16 @@ const handleLogin = async (req, res) => {
     const match = await bcrypt.compare(pwd, foundUser.password);
     if (match) {
         // create JWTs
+        const accessToken = jwt.sign(
+            { "username":foundUser.username },
+            process.env.ACCESS_TOKEN_SECRET,
+            { expiresIn: '30s' }
+        );
+        const refreshToken = jwt.sign(
+            { "username":foundUser.username },
+            process.env.REFRESH_TOKEN_SECRET,
+            { expiresIn: '1d' }
+        );
         res.json({ 'success': `User ${user} is logged in!` });
     } else {
         res.sendStatus(401);
