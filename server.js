5a82be622b35d4e292bacb5ad47d61abe2764e7e
Patrick Oser <patrick.oser148@gmail.com>
2023-11-05 11:53:33 -0500

    Call 'credentials' in 'server.js'.

--- server.js
+++ server.js
@@ -7,11 +7,16 @@ const { logger } = require('./middleware/logEvents');
 const errorHandler = require('./middleware/errorHandler');
 const cookieParser = require('cookie-parser');
 const verifyJWT = require('./middleware/verifyJWT');
+const credentials = require('./middleware/credentials');
 const PORT = process.env.PORT || 3500;
 
 // custom middleware logger
 // next() needs to be called because this is not bult in middleware
 app.use(logger);
+
+// Handle options credentials check - before CORS!
+// and fetch cookies credentials requirement
+app.use(credentials);
 
 // Cross Origin Resource Sharing
 app.use(cors(corsOptions));
