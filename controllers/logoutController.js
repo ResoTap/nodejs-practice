37df62e98226b49411675d289d6d6fa25ce1cace
Patrick Oser <patrick.oser148@gmail.com>
2023-11-05 10:49:31 -0500

    Add 'logout.js' file to 'routes' folder.

---
+++ controllers/logoutController.js
@@ -0,0 +1,33 @@
+const usersDB = {
+    users: require('../model/users.json'),
+    setUsers: function (data) { this.users = data }
+}
+const fsPromises = require('fs').promises;
+const path = require('path');
+
+const handleLogout = async (req, res) => {
+    // On client, also delete the access token
+
+    const cookies = req.cookies;
+    if (!cookies?.jwt) return res.sendStatus(204); // successful, no content to send
+    const refreshToken = cookies.jwt;
+
+    // Is refreshToken in db
+    const foundUser = usersDB.users.find(person => person.refreshToken === refreshToken);
+    if (!foundUser) {
+        res.clearCookie('jwt', { httpOnly: true });
+        return res.sendStatus(403);
+    }
+    // Delete refreshToken in the db
+    const otherUsers = usersDB.users.filter(person => person.refreshToken !== foundUser.refreshToken);
+    const currentUser = {...foundUser, refreshToken: ''};
+    usersDB.setUsers([...otherUsers, currentUser]);
+    await fsPromises.writeFile(
+        path.join(__dirname, '..', 'model', 'users.json'),
+        JSON.stringify(usersDB.users)
+    );
+
+    res.clearCookie('jwt', { httpOnly: true }); // secure: true - only serces https
+    res.sendStatus(204);
+}
+module.exports = { handleLogout };
