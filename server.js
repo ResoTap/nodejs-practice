120d661db5f1b318701553be1f5117534621718a
Patrick Oser <patrick.oser148@gmail.com>
2023-11-08 14:16:37 -0500

    'useNewUrlParser' and 'useUnifiedTopology' deprecated in 'connectDB'.

--- server.js
+++ server.js
@@ -61,4 +61,7 @@ app.all('*', (req, res) => {
 
 app.use(errorHandler);
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));    
+mongoose.connection.once('open', () => {
+    console.log('Connected to MongoDB');
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`)); 
+})   
