344e558ff7684d8dfc562a4fb7265dd7caa766fa
Patrick Oser <patrick.oser148@gmail.com>
2023-11-08 13:07:47 -0500

    Add '.env' to server.js and remove from all sub folders.

--- controllers/refreshTokenController.js
+++ controllers/refreshTokenController.js
@@ -4,13 +4,12 @@ const usersDB = {
 }
 
 const jwt = require('jsonwebtoken');
-require('dotenv').config();
 
 const handleRefreshToken = (req, res) => {
     const cookies = req.cookies;
     if (!cookies?.jwt) return res.sendStatus(401);
     const refreshToken = cookies.jwt;
-    
+
     const foundUser = usersDB.users.find(person => person.refreshToken === refreshToken);
     if (!foundUser) return res.sendStatus(403); // Forbidden
     // evaluate jwt
